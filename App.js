c7b924dea333d42124264e2576ca033a3b99af97
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-12 22:47:10 +0200

    Implemented images

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log(x);
   return (
-    <View style={styles.container}>
-      <Text>Hello Native App :)</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text> I love u sibgha. Hello Native App :)</Text>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
   },
