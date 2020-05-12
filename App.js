86c6dfcfd114f146db6b8012e92109ec3478797e
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-13 01:39:20 +0200

    Built the button component

--- App.js
+++ App.js
@@ -8,14 +8,19 @@ import {
 } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={styles.container}>
+      <AppButton title="Login" onPress={() => console.log("tapped")} />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
