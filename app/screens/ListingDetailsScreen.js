e9f47466c6d50798360bbbe695e22f886f31ce14
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-13 04:08:51 +0200

    Implemented ListItem component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,20 +2,26 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import colors from "../config/colors";
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 
-const ListingDetailsScreen = () => {
+function ListingDetailsScreen() {
   return (
     <View>
       <Image style={styles.image} source={require("../assets/jacket.jpg")} />
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red Jacket for sale</AppText>
         <AppText style={styles.price}>100$</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            title="Ubaid Rana"
+            subTitle=" 5 Listing"
+            image={require("../assets/ubaid.jpg")}
+          />
+        </View>
       </View>
     </View>
   );
-};
-
-export default ListingDetailsScreen;
+}
 
 const styles = StyleSheet.create({
   image: {
@@ -36,4 +42,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 10,
   },
+  userContainer: {
+    marginVertical: 40,
+  },
 });
+
+export default ListingDetailsScreen;
