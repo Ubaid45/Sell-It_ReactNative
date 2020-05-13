c4709d784190e9257db3c6b494a6051e7f26fb62
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-13 14:12:43 +0200

    Built the re-useable icon component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" />
+    </Screen>
+  );
 }
