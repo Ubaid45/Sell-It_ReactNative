ceee709bc57fbcdf557ff0fad2525a1d335019c2
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-14 15:55:03 +0200

    Implemented flexible architecture to render either a flat list or fancy category picker

---
+++ app/components/CategoryPickerItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+
+function CategoryPickerItem({ item, onPress }) {
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+});
+
+export default CategoryPickerItem;
--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,7 +4,13 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, width, placeholder }) {
+function AppFormPicker({
+  items,
+  name,
+  PickerItemComponent,
+  width,
+  placeholder,
+}) {
   const { errors, setFieldValue, touched, values } = useFormikContext();
 
   return (
@@ -12,6 +18,7 @@ function AppFormPicker({ items, name, width, placeholder }) {
       <AppPicker
         items={items}
         onSelectItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
         placeholder={placeholder}
         selectedItem={values[name]}
         width={width}
