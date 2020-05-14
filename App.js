0ab2d624880c248f5a1b424efbabe8cdb3ff4bad
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-14 05:45:26 +0200

    Deleted wrongly commited files

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/assets/Ultimate React Native- Part 1/Section 7- Forms/End/app/screens/RegisterScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <RegisterScreen />;
+  return <ListingEditScreen />;
 }
