a5d8581f36c625c5393c42313ef9f3212db72011
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-14 06:02:45 +0200

    Re-organized the Listing components

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import LoginScreen from "./app/screens/LoginScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import colors from "../config/colors";
+import { Image, StyleSheet, View } from "react-native";
+
 import AppText from "../components/AppText";
-import ListItem from "../components/ListItem";
+import ListItem from "../components/lists/ListItem";
+import colors from "../config/colors";
 
 function ListingDetailsScreen() {
   return (
