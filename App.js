f5c635f15f4654c9dea5c799eff8f18481f9e1de
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-14 05:28:47 +0200

    Implemented register screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/assets/Ultimate React Native- Part 1/Section 7- Forms/End/app/screens/RegisterScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
